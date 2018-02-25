059e3013e64e8bb1333e3f5815a281d6fe183db5
David Elner <david@davidelner.com>
2018-02-24 20:29:16 -0500

    Added: Code coverage to RSpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
-require "bundler/setup"
-require "skeletron"
+require 'bundler/setup'
+require 'simplecov'
+require 'pry'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
@@ -12,3 +13,7 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+# Start code coverage
+SimpleCov.start
+require 'skeletron'
